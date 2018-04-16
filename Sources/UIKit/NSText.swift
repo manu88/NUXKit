778329b33550a3a7e9b1d025ed75cdff8152a9e1
Manuel Deneu <manueldeneu@gmail.com>
2018-04-16 19:35:27 +0200

    Added UITextField class

---
+++ Sources/UIKit/NSText.swift
@@ -0,0 +1,9 @@
+//
+//  NSText.swift
+//  NUXKit
+//
+//  Created by Manuel Deneu on 16/04/2018.
+//  Copyright © 2018 Manuel Deneu. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/UIKit/UITextField.swift
@@ -0,0 +1,9 @@
+//
+//  UITextField.swift
+//  NUXKit
+//
+//  Created by Manuel Deneu on 16/04/2018.
+//  Copyright © 2018 Manuel Deneu. All rights reserved.
+//
+
+import Foundation
